3fa2c87c75aa0bcfd5de009c455f3b11a4ede56c
KuznetsovKU <116377097+KuznetsovKU@users.noreply.github.com>
2023-04-11 11:05:53 +0300

    Update Homework_1/Ex_7.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- Homework_1/Ex_7.py
+++ Homework_1/Ex_7.py
@@ -7,7 +7,7 @@ year_num = int(input('Введите натуральное число: '))
 
 
 def is_leap_year(year):
-    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
         print('YES')
     else:
         print('NO')
